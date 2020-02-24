048adab37622d4f3dee754d9f7f8c06093073ee7
Tomáš Kulhavý <u17.414@pslib.cloud>
2020-02-24 12:22:33 +0100

    new reborn

---
+++ BattleShips/Services/IGame.cs
@@ -0,0 +1,19 @@
+﻿using BattleShips.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleShips.Services
+{
+    public interface IGame
+    {
+        IEnumerable<User> GetAllUsers();
+        int Size { get; set; }
+        int Players { get; set; }
+        bool RemoveGame(Guid Id);
+        bool AddGame(Game Game);
+        //AddUser
+        //ShipPlacement       
+    }
+}
