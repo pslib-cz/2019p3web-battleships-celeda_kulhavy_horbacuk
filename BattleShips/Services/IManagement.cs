8c47b4035d60337f4f49444e38eb229319411682
Tomáš Kulhavý <tomas.kulhavy@pslib.cz>
2020-03-01 11:05:57 +0100

    IManagement

--- BattleShips/Services/IManagement.cs
+++ BattleShips/Services/IManagement.cs
@@ -10,9 +10,16 @@ namespace BattleShips.Services
     {
         //vytvoreni novy hry, prihlasovani, vypis, odhlasovani, hlavni stranka 
         //Kulhavý
-        IEnumerable<User> GetAllUsers();
-        IEnumerable<Game> GetGames();
+        IEnumerable<User> GetUsers(string userId);
+        IEnumerable<Game> GetGames(Guid gameId);
+        IEnumerable<Game> GetGames(int? gameFilter);
+        List<Game> GetGameDetails(Guid gameId);
         bool RemoveGame(Guid Id);
         bool AddGame(Game Game);
+        bool AddUser(User user);
+        bool RemoveUserFromGame(string userId);
+        Game CreateNewGame(string userId, Guid gameId, int maxPlayers, int boardSize);
+        User CreateNewUser(string userId, string Name);
+        UserGame CreateUserGame(string userId, Guid gameId);
     }
 }
