80f9c9f6ff18503a958b9f5ec5d17d764085a333
Horbachuk Lyubomyr <drzoidberh18@gmail.com>
2020-03-02 09:13:21 +0100

    ISetup upgrade

--- BattleShips/Services/ISetup.cs
+++ BattleShips/Services/ISetup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BattleShips.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,10 @@ namespace BattleShips.Services
 {
     public interface ISetup
     {
+        //Vytvařeni a rozmištěni vlastnich lodi na sve herni ploše
+        public Game GetGame(int id);
+        public UserGame GetUserGame(string userId, int gameId);
+        public void ShipsReady(); //Rozmistěni lodi na herni plochu
+        public void Ready(); //Konrola jestli všichni už jsou připraveny ke hře
     }
 }
