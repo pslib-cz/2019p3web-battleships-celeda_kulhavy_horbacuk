2711905ea41a587be7aa2d2e3a06f47a00403268
Tomáš Kulhavý <tomas.kulhavy@pslib.cz>
2020-04-27 12:05:08 +0200

    Pages

--- BattleShips/Pages/Game/InGame.cshtml.cs
+++ BattleShips/Pages/Game/InGame.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BattleShips.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,14 @@ namespace BattleShips.Pages.Game
 {
     public class InGameModel : PageModel
     {
+        public string Color { get; set; }
+        public ApplicationDbContext _db;
+        public InGameModel(ApplicationDbContext db)
+        {
+            _db = db;
+            Color = "black";
+        }
+
         public void OnGet()
         {
 
